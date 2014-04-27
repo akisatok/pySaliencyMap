b80486cb1c26b85982c63222886d435767ba81e5
Akisato Kimura <akisato@ieee.org>
2014-04-27 21:24:04 +0900

    This is the first commit. Welcome to the pySaliencyMap package!

---
+++ pySaliencyMap.py
@@ -0,0 +1,260 @@
+#-------------------------------------------------------------------------------
+# Name:        pySaliencyMap
+# Purpose:     Extracting a saliency map from a single still image
+#
+# Author:      Akisato Kimura <akisato@ieee.org>
+#
+# Created:     24/04/2014
+# Copyright:   (c) Akisato Kimura 2014-
+# Licence:     All rights reserved
+#-------------------------------------------------------------------------------
+
+import cv2, sys
+import numpy as np
+import scipy as sp
+import pySaliencyMapDefs
+
+class pySaliencyMap:
+    # initialization
+    def __init__(self, width, height):
+        self.width  = width
+        self.height = height
+        self.prev_frame = None
+        self.GaborKernel0   = np.array(pySaliencyMapDefs.GaborKernel_0)
+        self.GaborKernel45  = np.array(pySaliencyMapDefs.GaborKernel_45)
+        self.GaborKernel90  = np.array(pySaliencyMapDefs.GaborKernel_90)
+        self.GaborKernel135 = np.array(pySaliencyMapDefs.GaborKernel_135)
+
+    # extracting color channels
+    def SMExtractRGBI(self, inputImage):
+        # convert scale of array elements
+        src = np.float32(inputImage) * 1./255
+        # split
+        (B, G, R) = cv2.split(src)
+        # extract an intensity image
+        I = cv2.cvtColor(src, cv2.cv.CV_BGR2GRAY)
+        # return
+        return R, G, B, I
+
+    # feature maps
+    ## constructing a Gaussian pyramid
+    def FMCreateGaussianPyr(self, src):
+        dst = []
+        dst.append(src)
+        for i in range(1,9):
+            nowdst = cv2.pyrDown(dst[i-1])
+            dst.append(nowdst)
+        return dst
+    ## taking center-surround differences
+    def FMCenterSurroundDiff(self, GaussianMaps):
+        dst = []
+        for s in range(2,5):
+            now_size = GaussianMaps[s].shape
+            now_width  = now_size[1]
+            now_height = now_size[0]
+            tmp = cv2.resize(GaussianMaps[s+3], (now_width, now_height), interpolation=cv2.INTER_LINEAR)
+            nowdst = cv2.absdiff(GaussianMaps[s], tmp)
+            dst.append(nowdst)
+            tmp = cv2.resize(GaussianMaps[s+4], (now_width, now_height), interpolation=cv2.INTER_LINEAR)
+            nowdst = cv2.absdiff(GaussianMaps[s], tmp)
+            dst.append(nowdst)
+        return dst
+    ## constructing a Gaussian pyramid + taking center-surround differences
+    def FMGaussianPyrCSD(self, src):
+        GaussianMaps = self.FMCreateGaussianPyr(src)
+        dst = self.FMCenterSurroundDiff(GaussianMaps)
+        return dst
+    ## intensity feature maps
+    def IFMGetFM(self, I):
+        return self.FMGaussianPyrCSD(I)
+    ## color feature maps
+    def CFMGetFM(self, R, G, B):
+        # max(R,G,B)
+        tmp1 = cv2.max(R, G)
+        RGBMax = cv2.max(B, tmp1)
+        RGBMax = cv2.max(RGBMax, 0.0001)    # prevent dividing by 0
+        # min(R,G)
+        RGMin = cv2.min(R,G)
+        # RG = (R-G)/max(R,G,B)
+        RG = (R - G) / RGBMax
+        # BY = (B-min(R,G)/max(R,G,B)
+        BY = (B - RGMin) / RGBMax
+        # clamp nagative values to 0
+        RG = cv2.max(RG, 0)
+        BY = cv2.max(BY, 0)
+        # obtain feature maps in the same way as intensity
+        RGFM = self.FMGaussianPyrCSD(RG)
+        BYFM = self.FMGaussianPyrCSD(BY)
+        # return
+        return RGFM, BYFM
+    ## orientation feature maps
+    def OFMGetFM(self, src):
+        # creating a Gaussian pyramid
+        GaussianI = self.FMCreateGaussianPyr(src)
+        # convoluting a Gabor filter with an intensity image to extract oriemtation features
+        GaborOutput0   = [ np.empty((1,1)), np.empty((1,1)) ]  # dummy data: any kinds of np.array()s are OK
+        GaborOutput45  = [ np.empty((1,1)), np.empty((1,1)) ]
+        GaborOutput90  = [ np.empty((1,1)), np.empty((1,1)) ]
+        GaborOutput135 = [ np.empty((1,1)), np.empty((1,1)) ]
+        for j in range(2,9):
+            GaborOutput0.append(   cv2.filter2D(GaussianI[j], cv2.CV_32F, self.GaborKernel0) )
+            GaborOutput45.append(  cv2.filter2D(GaussianI[j], cv2.CV_32F, self.GaborKernel45) )
+            GaborOutput90.append(  cv2.filter2D(GaussianI[j], cv2.CV_32F, self.GaborKernel90) )
+            GaborOutput135.append( cv2.filter2D(GaussianI[j], cv2.CV_32F, self.GaborKernel135) )
+        # calculating center-surround differences for every oriantation
+        CSD0   = self.FMCenterSurroundDiff(GaborOutput0)
+        CSD45  = self.FMCenterSurroundDiff(GaborOutput45)
+        CSD90  = self.FMCenterSurroundDiff(GaborOutput90)
+        CSD135 = self.FMCenterSurroundDiff(GaborOutput135)
+        # concatenate
+        dst = list(CSD0)
+        dst.extend(CSD45)
+        dst.extend(CSD90)
+        dst.extend(CSD45)
+        # return
+        return dst
+    ## motion feature maps
+    def MFMGetFM(self, src):
+        # convert scale
+        I8U = np.uint8(src) * 255
+        # calculating optical flows
+        if self.prev_frame != None:
+            param1 = pySaliencyMapDefs.farne_pyr_scale
+            param2 = pySaliencyMapDefs.farne_levels
+            param3 = pySaliencyMapDefs.farne_winsize
+            param4 = pySaliencyMapDefs.farne_iterations
+            param5 = pySaliencyMapDefs.farne_poly_n
+            param6 = pySaliencyMapDefs.farne_poly_sigma
+            param7 = pySaliencyMapDefs.farne_flags
+            flow = cv2.calcOpticalFlowFarneback(prev_frame, I8U, \
+                    param1, param2, param3, param4, param5, param6, param7)
+            flowx = flow[...,0]
+            flowy = flow[...,1]
+        else:
+            flowx = np.zeros(I8U.shape)
+            flowy = np.zeros(I8U.shape)
+        # create Gaussian pyramids
+        dst_x = self.FMGaussianPyrCSD(flowx)
+        dst_y = self.FMGaussianPyrCSD(flowy)
+        # update the current frame
+        self.prev_frame = np.array(I8U)
+        # return
+        return dst_x, dst_y
+    # conspicuity maps
+    ## standard range normalization
+    def SMRangeNormalize(self, src):
+        minn, maxx, nouse1, nouse2 = cv2.minMaxLoc(src)
+        if maxx!=minn:
+            dst = src/(maxx-minn) + minn/(minn-maxx)
+        else:
+            dst = src - minn
+        return dst
+    ## computing an average of local maxima
+    def SMAvgLocalMax(self, src):
+        # size
+        stepsize = pySaliencyMapDefs.default_step_local
+        width = src.shape[1]
+        height = src.shape[0]
+        # find local maxima
+        numlocal = 0
+        lmaxmean = 0
+        for y in range(0, height-stepsize, stepsize):
+            for x in range(0, width-stepsize, stepsize):
+                localimg = src[y:y+stepsize, x:x+stepsize]
+                lmin, lmax, nouse1, nouse2 = cv2.minMaxLoc(localimg)
+                lmaxmean += lmax
+                numlocal += 1
+        # averaging over all the local regions
+        return lmaxmean / numlocal
+    ## normalization specific for the saliency map model
+    def SMNormalization(self, src):
+        dst = self.SMRangeNormalize(src)
+        lmaxmean = self.SMAvgLocalMax(dst)
+        normcoeff = (1-lmaxmean)*(1-lmaxmean)
+        return dst * normcoeff
+    ## normalizing feature maps
+    def normalizeFeatureMaps(self, FM):
+        NFM = []
+        for i in range(0,6):
+            normalizedImage = self.SMNormalization(FM[i])
+            nownfm = cv2.resize(normalizedImage, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
+            NFM.append(nownfm)
+        return NFM
+    ## intensity conspicuity map
+    def ICMGetCM(self, IFM):
+        NIFM = self.normalizeFeatureMaps(IFM)
+        return NIFM[0]
+    ## color conspicuity map
+    def CCMGetCM(self, CFM_RG, CFM_BY):
+        # extracting a conspicuity map for every color opponent pair
+        CCM_RG = self.ICMGetCM(CFM_RG)
+        CCM_BY = self.ICMGetCM(CFM_BY)
+        # merge
+        CCM = CCM_RG + CCM_BY
+        # return
+        return CCM
+    ## orientation conspicuity map
+    def OCMGetCM(self, OFM):
+        OCM = np.zeros((self.height, self.width))
+        for i in range (0,4):
+            # slicing
+            nowofm = OFM[i*6:(i+1)*6]  # angle = i*45
+            # extracting a conspicuity map for every angle
+            NOFM = self.ICMGetCM(nowofm)
+            # normalize
+            NOFM2 = self.SMNormalization(NOFM)
+            # accumulate
+            OCM += NOFM2
+        return OCM
+    ## motion conspicuity map
+    def MCMGetCM(self, MFM_X, MFM_Y):
+        return self.CCMGetCM(MFM_X, MFM_Y)
+
+    # core
+    def SMGetSM(self, src):
+        # definitions
+        size = src.shape
+        width  = size[1]
+        height = size[0]
+        # check
+        #if(width != self.width or height != self.height):
+        #    sys.exit("size mismatch")
+        # extracting individual color channels
+        R, G, B, I = self.SMExtractRGBI(src)
+        # extracting feature maps
+        IFM = self.IFMGetFM(I)
+        CFM_RG, CFM_BY = self.CFMGetFM(R, G, B)
+        OFM = self.OFMGetFM(I)
+        MFM_X, MFM_Y = self.MFMGetFM(I)
+        # extracting conspicuity maps
+        ICM = self.ICMGetCM(IFM)
+        CCM = self.CCMGetCM(CFM_RG, CFM_BY)
+        OCM = self.OCMGetCM(OFM)
+        MCM = self.MCMGetCM(MFM_X, MFM_Y)
+        # adding all the conspicuity maps to form a saliency map
+        wi = pySaliencyMapDefs.weight_intensity
+        wc = pySaliencyMapDefs.weight_color
+        wo = pySaliencyMapDefs.weight_orientation
+        wm = pySaliencyMapDefs.weight_motion
+        SMMat = wi*ICM + wc*CCM + wo*OCM + wm*MCM
+        # normalize
+        normalizedSM = self.SMRangeNormalize(SMMat)
+        smoothedSM = cv2.GaussianBlur(normalizedSM, (7,7), 1.55)
+        SM = cv2.resize(smoothedSM, (width,height), interpolation=cv2.INTER_NEAREST)
+        # return
+        return SM
+
+# main
+if __name__ == '__main__':
+    img = cv2.imread('test.jpg')
+    imgsize = img.shape
+    img_width  = imgsize[1]
+    img_height = imgsize[0]
+    sm = pySaliencyMap(img_width, img_height)
+    map = sm.SMGetSM(img)
+    cv2.imshow("input",  img)
+    cv2.imshow("output", map)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
